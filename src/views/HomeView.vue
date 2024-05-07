b91e7c267c06cf36a06acf1c5baf0052186a4b59
joao henryque <henryque_joao17@hotmail.com>
2024-05-07 10:22:04 -0300

    fix: ajustes na tela principal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,12 +34,27 @@
       </section>
       <section class="data-Prohibited">
         <h1>Entradas</h1>
-        <div>
-          <div v-for="(item, index) in itens" :key="index" class="card">
+        <div class="container-card">
+          <div v-for="(item, index) in tarefas" :key="index" class="card">
             <div class="card-body">
               <input type="checkbox" v-model="item.selected" class="check-input">
-              <h5 class="card-title">{{ item.titulo }}</h5>
-              <p class="card-text">{{ item.descricao }}</p>
+              <div class="content-card">
+                <h5 class="card-title">{{ item.title }}</h5>
+                <p class="card-text">{{ item.description }}</p>
+                <div class="date-input">
+                  <span class="date-icon">
+                    <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="24" 
+                    viewBox="0 -960 960 960" 
+                    width="24">
+                    <path 
+                    d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/>
+                    </svg>
+                  </span>
+                  <input type="date" class="date-field">
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -59,21 +74,86 @@ export default {
   data() {
     return {
       itens: [
-        { titulo: 'Item 1', descricao: 'Descrição do Item 1' },
+        { titulo: 'Item 1', descricao: 'Descrição do Item 1 ssadasdsadasdasdasdasdasdsdasdasdadasdasddadsadasdasdasdasdasdasdasdasdsadasdasd' },
         { titulo: 'Item 2', descricao: 'Descrição do Item 2' },
         { titulo: 'Item 3', descricao: 'Descrição do Item 3' }
-      ]
+      ],
+      tarefas:[],
     };
+  },
+
+  created() {
+     this.getTasks();
+  },
+
+  methods: {
+    truncarDescricao(descricao) {
+      const comprimentoMaximo = 95; // Defina o comprimento máximo desejado
+      if (descricao.length > comprimentoMaximo) {
+        return descricao.substring(0, comprimentoMaximo) + '...';
+      } else {
+        return descricao;
+      }
+    },
+    getTasks() {
+     this.$axios.get('tarefas')
+      .then((response) => {
+        this.tarefas = response.data.data
+        console.log(this.tarefas);
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
   }
 }
+
+
 </script>
 
 
 <style lang="scss" scoped>
 .home {
   width: 100%;
-  height: 100vh;
+  height: 98vh;
   background-color: #ffffff;
+
+}
+
+.container-card {
+  width: 1200px;
+  height: 600px; /* Altura máxima da div */
+  overflow-y: auto; /* Adiciona scroll vertical quando necessário */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Esconde a barra de rolagem no Chrome, Safari, etc */
+  }
+}
+
+.date-input {
+  position: relative;
+  display: inline-block;
+}
+
+input[type="date"]::-webkit-calendar-picker-indicator {
+  display: none;
+}
+
+.date-icon {
+  position: absolute;
+  left: 5px; /* Ajuste a posição horizontal do ícone */
+  transform: translateY(-50%);
+  z-index: 999;
+  top: -4px;
+  width: 24px; /* Ajuste a largura do ícone */
+  height: 24px; /* Ajuste a altura do ícone */
+  pointer-events: none; /* Garante que o ícone não seja clicável */
+}
+
+.date-field {
+  padding-left: 30px; /* Deixa espaço para o ícone */
+  height: 30px;
 
 }
 
@@ -84,10 +164,11 @@ export default {
   width: 25px;
   height: 25px;
   border-radius: 50%;
-  border: 2px solid #333;
+  border: 2px solid #E5E5E5;
   outline: none;
   cursor: pointer;
   position: relative;
+  top: -27px !important;
 }
 
 .check-input:checked::before {
@@ -98,29 +179,59 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: #000000; 
-  width: 99%; 
-  height: 99%; 
-  border-radius: 50%; 
+  background-color: #000000;
+  width: 99%;
+  height: 99%;
+  border-radius: 50%;
   text-align: center;
-  line-height: 20px; 
+  line-height: 20px;
 }
 
 
 .data-Prohibited {
   flex: 1;
-  height: 100vh;
-  padding-left: 30px;
+  height: 80vh;
+  padding-left: 250px;
+  padding-top: 110px;
+
+  h1 {
+    font-size: 35px !important;
+  }
+
 }
+
+.content-card {
+  position: relative;
+  left: 30px;
+}
+
+
 
 .card-body {
   width: 80%;
-  height: 110px;
+  height: 130px;
   border: 2px solid #E5E5E5;
-  display: grid;
+  display: flex;
+  margin-bottom: 20px;
   align-items: center;
-  justify-content: flex-start;
-  gap: 40px;
+  padding-left: 20px;
+
+  h5 {
+    font-size: 18px;
+
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 200;
+    position: relative;
+    top: -20px;
+  }
+
+  input {
+    position: relative;
+    top: -20px;
+  }
 }
 
 .data-general {
@@ -131,7 +242,7 @@ export default {
 
 .menu-left {
   width: 300px;
-  height: 100vh;
+  height: 90vh;
   background: #FAFAFA;
 }
 
@@ -146,7 +257,8 @@ export default {
   justify-content: start;
   align-items: center;
   padding-left: 30px;
-
+  position: relative;
+  top: 50px;
   .icon-menu {
     position: relative;
     top: 70px;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -68,8 +68,6 @@ export default {
   mounted() {
     this.$axios.baseURL;
     console.log(this.$route.params.id);
-
-
   },
   methods: {
     async login() {
